658167b82c1daa7b595df50fbb72baeb1542e094
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-02-03 12:51:07 -0800

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,21 @@
-import { useState } from "react";
-import {
-  ConfigProvider,
-  App as AntdApp,
-  Button,
-  Typography,
-  Layout,
-  notification,
-} from "antd";
+import { ConfigProvider, App as AntdApp, notification } from "antd";
 import { themeToken } from "./lib/theme-token";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Valentine from "./Valentine";
+
 import "./App.css";
 
-const { Content } = Layout;
-const { Title } = Typography;
-const comicalResponses = [
-  "Really? Try again.",
-  "Nope, still waiting...",
-  "Oh, come on!",
-  "Are you sure?",
-  "Give it another shot!",
-  "Almost convinced...",
-  "I'm not giving up!",
-  "You do realize I'll keep asking?",
-  "Yes is the right answer...",
-  "Just click Yes!",
-  "No means Yes, right?",
-];
-
 function App() {
-  const [api, contextHolder] = notification.useNotification();
-
-  const [noButtonText, setNoButtonText] = useState("No");
-
-  const handleNoClick = () => {
-    const responseIndex = Math.floor(Math.random() * comicalResponses.length);
-    setNoButtonText(comicalResponses[responseIndex % comicalResponses.length]);
-  };
-
-  const handleYesClick = () => {
-    api.success({
-      message: `Yay!`,
-      description: "You made the right choice! Happy Valentine's Day!",
-    });
-  };
-
   return (
     <ConfigProvider theme={themeToken}>
       <AntdApp>
-        {contextHolder}
-        <Layout className="app-layout">
-          <Content className="app-content">
-            <Title className="valentine-name" level={1}>
-              Becca
-            </Title>
-            <Title className="valentine-question" level={2}>
-              Will you be my Valentine?
-            </Title>
-            <div>
-              <Button
-                type="primary"
-                size="large"
-                onClick={handleYesClick}
-                style={{ marginRight: 10 }}
-              >
-                Yes
-              </Button>
-              <Button size="large" onClick={handleNoClick}>
-                {noButtonText}
-              </Button>
-            </div>
-          </Content>
-        </Layout>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Valentine />} />
+            <Route path="/:name" element={<Valentine />} />
+          </Routes>
+        </Router>
       </AntdApp>
     </ConfigProvider>
   );
---
+++ src/Valentine.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Button, Typography, Layout, notification } from "antd";
+import { useParams } from "react-router-dom";
+
+const { Content } = Layout;
+const { Title } = Typography;
+const comicalResponses = [
+  "Really? Try again.",
+  "Nope, still waiting...",
+  "Oh, come on!",
+  "Are you sure?",
+  "Give it another shot!",
+  "Almost convinced...",
+  "I'm not giving up!",
+  "You do realize I'll keep asking?",
+  "Yes is the right answer...",
+  "Just click Yes!",
+  "No means Yes, right?",
+];
+
+export default function Valentine() {
+  const { name } = useParams();
+
+  const [noButtonText, setNoButtonText] = useState("No");
+  const [api, contextHolder] = notification.useNotification();
+
+  const handleNoClick = () => {
+    const responseIndex = Math.floor(Math.random() * comicalResponses.length);
+    setNoButtonText(comicalResponses[responseIndex % comicalResponses.length]);
+  };
+
+  const handleYesClick = () => {
+    api.success({
+      message: `Yay!`,
+      description: "You made the right choice! Happy Valentine's Day!",
+    });
+  };
+  return (
+    <Layout className="app-layout">
+      {contextHolder}
+      <Content className="app-content">
+        <Title className="valentine-name" level={1}>
+          {name ? `Hey ${name}` : "Hey You"}
+        </Title>
+        <Title className="valentine-question" level={2}>
+          Will you be my Valentine?
+        </Title>
+        <div>
+          <Button
+            type="primary"
+            size="large"
+            onClick={handleYesClick}
+            style={{ marginRight: 10 }}
+          >
+            Yes
+          </Button>
+          <Button size="large" onClick={handleNoClick}>
+            {noButtonText}
+          </Button>
+        </div>
+      </Content>
+    </Layout>
+  );
+}
